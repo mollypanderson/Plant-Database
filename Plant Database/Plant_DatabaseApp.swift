e521d7b0b5366290d5088f5ed6354a344fcf3701
mollypanderson <mollypanderson@yahoo.com>
2024-03-06 14:33:07 -0800

    Initial Commit

---
+++ Plant Database/Plant_DatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_DatabaseApp.swift
+//  Plant Database
+//
+//  Created by Molly Anderson on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Plant_DatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
