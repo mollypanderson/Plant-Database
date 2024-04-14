890633a9f664798940349aecf92429a6db1efd09
mollypanderson <mollypanderson@yahoo.com>
2024-04-14 01:08:46 -0700

    Add sort by action sheet

--- Plant Database/Views/OverviewTab.swift
+++ Plant Database/Views/OverviewTab.swift
@@ -11,6 +11,9 @@ struct OverviewTab: View {
     @Environment(ModelData.self) var modelData
     @State private var showFavoritesOnly = false
     
+    @State private var showingSort = false
+        @State private var selection = "None"
+    
     var deepGreen = Color(red: 92.0 / 255, green: 134.0 / 255, blue: 81.0 / 255)
 
     var filteredPlants: [Plant] {
@@ -31,15 +34,39 @@ struct OverviewTab: View {
                 }
             }
             .animation(.default, value: filteredPlants)
-            .navigationTitle("My Plant Database")
+            .navigationTitle("Overview")
             .fontWeight(.regular)
             .foregroundStyle(deepGreen)
+            .toolbar {
+                Button {
+                    showingSort = true
+                } label: {
+                    Label("Sort", systemImage: "arrow.up.arrow.down")
+                }
+                .tint(Color(red: 92 / 255, green: 134 / 255, blue: 81 / 255))
+                .searchable(text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=$text@*/.constant("")/*@END_MENU_TOKEN@*/, placement: /*@START_MENU_TOKEN@*/.automatic/*@END_MENU_TOKEN@*/)
+            }
             
         }
         .fontWeight(.light)
+        .confirmationDialog("Sort by...", isPresented: $showingSort, titleVisibility: .visible) {
+                ForEach([
+                    "Date of Next Action",
+                    "Date Adopted (Ascending)",
+                    "Date Adopted (Descending)",
+                    "Alphabetical by Name",
+                    "Alphabetical by Species",
+                    "Custom (Drag & Drop)"
+                ], id: \.self) { sort in
+                    Button(sort) {
+                        selection = sort
+                    }
+                }
+            }
+        }
     }
-    //.tabItem { "Overview" }
-}
+        
+
 
 
 #Preview {
